838a4201c17f092694d501f39e0e9e74206a19a9
adpce <97443550+adpce@users.noreply.github.com>
2024-07-17 10:56:01 +0100

    add aliases, setup nvim as manpager/less

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias cat="batcat"
 alias cd="z"
 alias cdl='cd "$(fd -td . | sk)"'
 
-alias cfn="nvim $HOME/.dotfiles/nvim/init.vim"
+alias cfn="nvim $HOME/.dotfiles/nvim/init.lua"
 alias cfs="nvim $HOME/.dotfiles/starship/starship.toml"
 alias cfw="nvim $HOME/.dotfiles/wezterm/wezterm.lua"
 
@@ -17,3 +17,9 @@ alias lbry="$HOME/Downloads/prog/lbry.AppImage"
 alias pas="$HOME/scripts/pass.fish"
 alias pcsx2="$HOME/Downloads/prog/pcsx2.AppImage"
 alias yt="$HOME/scripts/youtube.bash"
+
+alias ssh="wezterm ssh"
+
+alias ff="clear; fastfetch -c $HOME/source/fastfetch/presets/examples/6.jsonc"
+
+alias less="$HOME/neovim/share/nvim/runtime/macros/less.sh"
