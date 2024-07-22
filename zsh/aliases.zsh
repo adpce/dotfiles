3a7b183e4f39392e1945b0efc39096fe4e4090f5
adpce <97443550+adpce@users.noreply.github.com>
2024-07-22 02:32:45 +0100

    zsh add functions for zoxide integration

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,25 +1,47 @@
+#replace ls with lsd
+alias ls="lsd"
+alias la="ls -a"
+alias ll="ls -l"
+
+#replace cat with batcat
 alias cat="batcat"
+
+#replace cd with zoxide
 alias cd="z"
+
+#cd into the selected option from a list of dirs below this one
 alias cdl='cd "$(fd -td . | sk)"'
 
+#make it easier to access config files
+alias cfa="nvim $HOME/.dotfiles/zsh/aliases.zsh"
 alias cfn="nvim $HOME/.dotfiles/nvim/init.lua"
 alias cfs="nvim $HOME/.dotfiles/starship/starship.toml"
 alias cfw="nvim $HOME/.dotfiles/wezterm/wezterm.lua"
 
-alias finit="veracrypt -t -d && sudo rmdir -v /media/* && sudo bleachbit -c --preset && windscribe disconnect && sudo shutdown now"
-
-alias la="ls -a"
-alias ll="ls -l"
-alias ls="lsd"
-
-alias lbry="$HOME/Downloads/prog/lbry.AppImage"
-
-alias pas="$HOME/scripts/pass.fish"
-alias pcsx2="$HOME/Downloads/prog/pcsx2.AppImage"
-alias yt="$HOME/scripts/youtube.bash"
-
-alias ssh="wezterm ssh"
-
+#clear fastfetch
 alias ff="clear; fastfetch -c $HOME/source/fastfetch/presets/examples/6.jsonc"
 
+#leave computer
+alias finit="veracrypt -t -d && sudo rmdir -v /media/* && sudo bleachbit -c --preset && windscribe disconnect && sudo shutdown now"
+
+#replace less with nvim
 alias less="$HOME/neovim/share/nvim/runtime/macros/less.sh"
+
+#use wezterm's ssh utility
+alias ssh="wezterm ssh"
+
+#easy access for scripts and appimages
+alias lbry="$HOME/Downloads/prog/lbry.AppImage"
+alias pcsx2="$HOME/Downloads/prog/pcsx2.AppImage"
+alias pas="$HOME/scripts/pass.fish"
+alias yt="$HOME/scripts/youtube.bash"
+
+#lf into common dirs
+lf1() {
+	lf $(zoxide query "${1}")
+}
+
+#as above, but cd
+cd1() {
+	cd $(zoxide query "${1}")
+}
