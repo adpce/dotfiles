fbc62c04e29572278e3c317da05b4345139e8796
adpce <97443550+adpce@users.noreply.github.com>
2025-01-13 23:17:06 +0000

    rust: add pastel, remove alacritty

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -10,7 +10,7 @@ then
 fi
 
 rustup update
-cargo install ripgrep skim fd-find du-dust procs git-delta hexyl tealdeer grex shellharden choose gitui eza
+cargo install ripgrep skim fd-find du-dust procs git-delta hexyl tealdeer grex shellharden choose gitui eza pastel
 cargo install --locked broot hyperfine starship bat
 cargo install --locked --all-features --git https://github.com/ms-jpq/sad --branch senpai
 
@@ -32,37 +32,5 @@ git submodule update --init --recursive
 ./get-deps
 cargo build --release
 sudo ln -svt "/usr/local/bin" "${SOURCECODE_DIR}/wezterm/target/release/wezterm"
-
-cd ..
-
-#alacritty
-if ! command -v alacritty
-then
-    git clone https://github.com/alacritty/alacritty.git
-fi
-
-cd "alacritty/" || exit
-git pull origin
-if uname -r | grep "Ubuntu"
-then
-    sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 scdoc
-fi
-cargo build --release
-
-#desktop files
-sudo cp target/release/alacritty /usr/local/bin
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
-
-#documentation
-sudo mkdir -p /usr/local/share/man/man1
-sudo mkdir -p /usr/local/share/man/man5
-scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
-scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
-
-sudo ln -svt "/usr/local/bin" "${SOURCECODE_DIR}/alacritty/target/release/alacritty"
 
 cd || exit
